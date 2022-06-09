33a026187b4e788cdaec33eb2b6bd6038a74b994
valkarov <88056645+valkarov@users.noreply.github.com>
2022-06-08 22:10:49 -0600

    Commit de todo lo q hizo Aurelio

---
+++ src/test/java/com/tarea1/TareaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
