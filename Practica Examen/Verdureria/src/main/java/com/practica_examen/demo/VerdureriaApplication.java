220bf0926cb1a57bffe655b46dbbeae7f7e8a333
LuisA841 <aguilarluis841@gmail.com>
2022-06-29 22:26:22 -0600

    Interfaz Tarea

---
+++ Practica Examen/Verdureria/src/main/java/com/practica_examen/demo/VerdureriaApplication.java
@@ -0,0 +1,13 @@
+package com.practica_examen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerdureriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerdureriaApplication.class, args);
+	}
+
+}
---
+++ Practica Examen/Verdureria/src/test/java/com/practica_examen/demo/VerdureriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica_examen.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerdureriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
